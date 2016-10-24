c443f8a51121787dfe08a9a9d6d9235177bb1c13
JonaC <jonas.ba.carlsson@gmail.com>
2016-10-24 21:28:53 +0200

    added ranomdizing to cards

--- imports/ui/components/main.js
+++ imports/ui/components/main.js
@@ -14,8 +14,29 @@ export default class main extends Component {
     this.refs.scoreboard.incrementScore()
   }
 
+  createRandomList(list, row,col){
+    list2 = list.slice(); // make a copy of a list
+    list.push.apply(list,list2); // create a list with 2 of every element
+    console.log(list.length)
+    let len = list.length;
+    let ret = [];
+    for(i = 0; i < row ; i++){
+      let part = [];
+      for( j=0; j < col; j++){
+        index = Math.floor(Math.random() * list.length)  
+        part.push(list.splice(index,1)[0])
+      }
+      console.log(part)
+      ret.push(part)
+    }
+    console.log(ret)
+    return ret;
+    
+  }
+
   render() {
     //TODO: setup ranomdly generated lists
+    const cardMatrix1= this.createRandomList(['a','b','c','d','e','f','g','h'], 4, 4)
     const cardMatrix= [['a','b','c','d'],['e','f','g','h'],['a','b','c','d'],['e','f','g','h']];
     return (
       <div>
